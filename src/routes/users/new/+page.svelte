894b199414669649dcb8752d6fdfcde403806484
Timothy Kok <timothykok@MacBook-Pro-5.local>
2023-11-27 23:14:07 +0800

    first commit

---
+++ src/routes/users/new/+page.svelte
@@ -0,0 +1,107 @@
+<script>
+    import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+    import { goto } from '$app/navigation';
+    // import { authenticateUser }
+    let formErrors = {};
+    
+  
+
+    export function postSignUp() {
+      goto('/');
+    }
+  
+    export async function createUser(evt) {
+      evt.preventDefault()
+  
+      if (evt.target['password'].value != evt.target['password-confirmation'].value) {
+        formErrors['password'] = { message: 'Password confirmation does not match' };
+        return;
+      }
+  
+      const userData = {
+        name: evt.target['name'].value,
+        email: evt.target['email'].value,
+        password: evt.target['password'].value,
+      };
+      
+      const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/users', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      });
+  
+      if (resp.status == 200) {
+            postSignUp();
+        }
+       else {
+        const res = await resp.json();
+        formErrors = res.data;
+      }
+    }
+  </script>
+  
+
+  <div style= padding:3em>
+    <h1 class="text-center text-xl">Create An Account To Upload Pictures</h1>
+    <div class="text-center">
+        <a class="link-hover italic text-xs" href="/users/login">Already have an account? Click here to login instead.</a>
+    </div>
+    <div class="flex justify-center items-center mt-8">
+        <form on:submit={createUser} class="w-1/3">
+            <div class="form-control w-full">
+                <label class="label" for="name">
+                    <span class="label-text">Name</span>
+                </label>
+                <input type="text" name="name" placeholder="johndoe" class="input input-bordered w-full" />
+                {#if 'name' in formErrors}
+                <label class="label" for="name">
+                    <span class="label-text-alt text-red-500">{formErrors['name'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="email">
+                    <span class="label-text">Email</span>
+                </label>
+                <input type="email" name="email" placeholder="john@example.com" class="input input-bordered w-full" required />
+                {#if 'email' in formErrors}
+                <label class="label" for="email">
+                    <span class="label-text-alt text-red-500">{formErrors['email'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="password">
+                    <span class="label-text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="" class="input input-bordered w-full" required />
+                {#if 'password' in formErrors}
+                <label class="label" for="password">
+                    <span class="label-text-alt text-red-500">{formErrors['password'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="password">
+                    <span class="label-text">Confirm Password</span>
+                </label>
+                <input type="password" name="password-confirmation" placeholder="" class="input input-bordered w-full" required />
+                {#if 'password' in formErrors}
+                <label class="label" for="password">
+                    <span class="label-text-alt text-red-500">{formErrors['password'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full mt-4">
+                <button class="btn btn-md">Create an Account</button>
+            </div>
+        </form>
+    </div>
+</div>
